a4fcc095aa83743a7735b4d89cb5a95561cba0e4
Petro648 <54951191+Petro648@users.noreply.github.com>
2019-10-25 00:48:13 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project" {  
+description = "project id"
+default     = "terraform-1-125"
+}
+
+variable "region" {
+description = "region"
+default = "europe-west4"
+}
+
+variable "zone" {
+description = "zone"
+default = "europe-west4-a"
+}
